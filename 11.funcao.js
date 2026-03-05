ac24ebebac6f76fd6489fd619f73e4248abdb4cc
erik.f.santos6@ba.estudante.senai.br <aluno.den@senaibahia.com.br>
2026-03-05 16:45:51 -0300

    DBZ

---
+++ 11.funcao.js
@@ -0,0 +1,15 @@
+// Função comum no Javascript
+
+function somar(a, b) {
+    return a + b
+}
+
+const soma = somar(2, 3)
+const Subtração = subtrair(2, 3)
+const Multiplicacão = somar(2, 3)
+const Divisão = somar(2, 3)
+
+console.log("Soma: ", soma)
+console.log("Subtração: ", subtracao)
+console.log("Multiplicação: ", multiplicação)
+console.log("Divisão: ", divisão)
---
+++ 9.operadores.js
@@ -0,0 +1,21 @@
+//Operadores lógicos.
+
+let idade = 10
+let peso = "10"
+
+console.log(" - COMPARAÇÃO -")
+console.log(" comparação conteúdo:")
+console.log(idade == peso) 
+
+console.log("\nComparando tipo e conteúdo:")
+console.log("Idade e peso são iguais?")
+console.log(idade == peso)
+
+console.log(" - DIFERENÇA -")
+console.log(" \ncomparando conteúdo:")
+console.log("Idade e peso são diferentes?") 
+console.log(idade != peso)  
+
+console.log("\nComparando tipo e conteúdo:")
+console.log("Idade e peso são difrentes?")
+console.log(idade !== peso)
