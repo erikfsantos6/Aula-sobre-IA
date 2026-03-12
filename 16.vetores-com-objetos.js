2872197d90cc9190ce92ac5a7b6d74211e408f98
erik.f.santos6@ba.estudante.senai.br <aluno.den@senaibahia.com.br>
2026-03-12 16:00:09 -0300

    IA

--- 16.vetores-com-objetos.js
+++ 16.vetores-com-objetos.js
@@ -36,3 +36,20 @@ const maioridade = listaDeUsuarios.filter(usuario => usuario.idade >= 18)
 maioridade.forEach(usuario => 
     console.log(`${usuario.nome} tem ${usuario.idade} anos.`)
 )
+
+// 2. Na lista de números, filtre e mostre apenas números pares
+console.log("\nNa lista de números, filtre e mostre apenas numeros pares.")
+const numerosPares = listaDeNumeros.filter(numero => numero % 2 === 0)
+console.log(numerosPares)
+
+// 3. Na lista de nomes, mostre todos os nomes com forEach
+console.log("\nNa lista de nomes, mostre todos os nomes com forEach.")
+listaDeNomes.forEach(nome => {
+    console.log(nome)
+})
+
+// 4. Na lista de usuários, mostre apenas os nomes
+console.log("\nNa lista de usuários, mostre apenas os nomes.")
+listaDeUsuarios.forEach(usuario => {
+    console.log(usuario.nome)
+})
