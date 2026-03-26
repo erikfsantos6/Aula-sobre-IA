9e1b1a8a297616152ead3afe21504afeebf39602
erik.f.santos6@ba.estudante.senai.br <aluno.den@senaibahia.com.br>
2026-03-26 16:54:12 -0300

    IA

---
+++ projeto/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Mensagem from './components/Mensagem'
+
+function App() {
+  
+  return (
+    <>
+    
+      <h1>Bem vindo ao meu</h1>
+
+      <Mensagem/>
+    </>
+  )
+}
+
+export default App
---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+function Mensagem() {
+    return (
+    // Comandos HTML
+    <div>
+        <h1>Componets</h1>
+        <p>Estes é um components com uma mensagem simples.</p>
+    </div>
+    )    
+}
+ export default Mensagem
